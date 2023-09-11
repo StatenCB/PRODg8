eff09fda1ddea8a72f94f5f37c822ec5d5f93863
Jonathan Rubensson <jonathan@eldfluga.se>
2023-09-11 12:08:02 +0200

    Auto stash before rebase of "origin/main"

---
+++ Source/ResidenceEvil/Room.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Room.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+ARoom::ARoom()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RoomBounds = CreateDefaultSubobject<UBoxComponent>(TEXT("RoomBounds"));
+	RootComponent = RoomBounds;
+}
+
+void ARoom::StartFire()
+{
+	bFireStarted = true;
+}
+
+void ARoom::SpreadFire()
+{
+	for(ARoom* Room : NeighboringRooms)
+	{
+		Room->StartFire();
+	}
+}
+
+// Called when the game starts or when spawned
+void ARoom::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARoom::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
